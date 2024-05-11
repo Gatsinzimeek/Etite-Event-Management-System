12f977aecb66b2798d5d9edb1d8e2607feba0e6a
GatsinziMeek <hackergastinzi@gmail.com>
2024-05-11 23:58:32 +0200

    new features

--- Client/src/Component/Top-Navigation-Bar/TopNavBar.jsx
+++ Client/src/Component/Top-Navigation-Bar/TopNavBar.jsx
@@ -1,18 +1,29 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons'
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import {faSearch, faBell, faChevronDown} from '@fortawesome/free-solid-svg-icons'
+import profile1 from '../../assets/profile.jpg'
 const TopNavBar = () => {
   return (
-    <div className='border-[#c1c1c1] border-b-[1px] p-5 flex justify-between'>
+    <div className='bg-white p-5 flex justify-between'>
       <div>
         <h1>Welcome</h1>
       </div>
-      <div className='flex items-center'>
+      <div className='flex items-center '>
         <div className='relative'>
-        <input type="text" placeholder='Search Here ...' className='px-[13px] py-[5px] outline-none rounded-2xl border' />
+        <input type="text" placeholder='Search Here ...' className='px-[13px] py-[5px] outline-none rounded-2xl border bg-slate-100' />
         <FontAwesomeIcon icon={faSearch} className='absolute right-3 hover:text-blue-400 hover:cursor-pointer top-2 '/>
         </div>
-      <FontAwesomeIcon icon={faBell} className='cursor-pointer'/>
+        <div className='relative'>
+            <FontAwesomeIcon icon={faBell} className='cursor-pointer  bg-slate-100 p-2 mx-2 rounded-2xl'/>
+            <span className='absolute bottom-6 text-[15px] right-1 bg-green-400  text-white rounded-2xl px-[5px] pt-[1px] cursor-pointer'>0</span>
+        </div>
+        <div className='flex justify-center gap-2'>
+            <img src={profile1} alt="profile"  className='w-[15%] h-[25%] rounded-[50%]'/>
+            <div>
+              <h6>Jessi Rose</h6>
+              <p>Admin</p>
+            </div>
+            <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon>
+        </div>
       </div>
       
     </div>
--- Client/src/Routes/Admin Dashboard/Admin.jsx
+++ Client/src/Routes/Admin Dashboard/Admin.jsx
@@ -3,8 +3,8 @@ import Navigation from "../../Component/Left-Navigation-Bar/Navigation";
 import TopNavBar from "../../Component/Top-Navigation-Bar/TopNavBar";
 const Admin = () => {
   return (
-    <main className="flex justify-around">
-        <nav className="border-[#c1c1c1] border-r-[1px] w-[15%] p-4 flex flex-col items-center">
+    <main className="flex justify-around bg-slate-100">
+        <nav className=" w-[15%] p-4 flex flex-col items-center">
             <Navigation></Navigation>
         </nav>
         <section className="flex-1 min-h-[100vh] flex flex-col">
