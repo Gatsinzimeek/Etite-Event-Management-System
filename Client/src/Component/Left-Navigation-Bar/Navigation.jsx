e6db97a8daafd32fbab685894fb9e3e912e1e7c6
GatsinziMeek <hackergastinzi@gmail.com>
2024-05-13 00:58:02 +0200

    new feature

--- Client/src/Component/Left-Navigation-Bar/Navigation.jsx
+++ Client/src/Component/Left-Navigation-Bar/Navigation.jsx
@@ -1,11 +1,68 @@
+import {
+  faHome,
+  faLocationDot,
+  faBus,
+  faUser,
+  faCircleInfo,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 
 const Navigation = () => {
+  const Navbar = [
+    {
+      Icon: faHome,
+      Title: "Dashboard",
+      path: "/dashboard/home",
+    },
+    {
+      Icon: faBus,
+      Title: "Events",
+      path: "/dashboard/Buses",
+    },
+
+    {
+      Icon: faLocationDot,
+      Title: "Tickets",
+      path: "/dashboard/Map",
+    },
+
+    {
+      Icon: faCircleInfo,
+      Title: "Details",
+      path: "/dashboard/Info",
+    },
+    {
+      Icon: faUser,
+      Title: "Profile",
+      path: "/dashboard/setting",
+    },
+  ];
+
   return (
     <div>
-        <ul>
-          <li><a href="">home</a></li>
-        </ul>
+      <ul>
+        {Navbar.map((nav, index) => (
+              <li
+                key={index}
+                className="p-[5px] px-[5px] mb-3 text-[15px] cursor-pointer hover:bg-[#3e61ac] hover:text-white rounded-xl mx-2"
+              >
+                <Link to={nav.path}>
+                  <FontAwesomeIcon
+                    icon={nav.Icon}
+                    className="pl-[10px] pr-[10px]"
+                  />
+                  {nav.Title}
+                </Link>
+              </li>
+            ))}
+      </ul>
+      <div>
+
+      </div>
     </div>
   )
 }
--- Client/src/Component/Top-Navigation-Bar/TopNavBar.jsx
+++ Client/src/Component/Top-Navigation-Bar/TopNavBar.jsx
@@ -3,29 +3,28 @@ import {faSearch, faBell, faChevronDown} from '@fortawesome/free-solid-svg-icons
 import profile1 from '../../assets/profile.jpg'
 const TopNavBar = () => {
   return (
-    <div className='bg-white p-5 flex justify-between'>
+    <div className='bg-white p-5 flex justify-between flex-wrap'>
       <div>
-        <h1>Welcome</h1>
+        <h1>Welcome </h1>
       </div>
-      <div className='flex items-center '>
+      <div className='flex items-center justify-center '>
         <div className='relative'>
-        <input type="text" placeholder='Search Here ...' className='px-[13px] py-[5px] outline-none rounded-2xl border bg-slate-100' />
+        <input type="text" placeholder='Search Here ...' className='px-[13px] py-[5px] w-[400px] outline-none rounded-2xl border bg-slate-100' />
         <FontAwesomeIcon icon={faSearch} className='absolute right-3 hover:text-blue-400 hover:cursor-pointer top-2 '/>
         </div>
         <div className='relative'>
             <FontAwesomeIcon icon={faBell} className='cursor-pointer  bg-slate-100 p-2 mx-2 rounded-2xl'/>
-            <span className='absolute bottom-6 text-[15px] right-1 bg-green-400  text-white rounded-2xl px-[5px] pt-[1px] cursor-pointer'>0</span>
+            <span className='absolute bottom-6 text-[15px] right-1 bg-green-400 shadow-lg  text-white rounded-2xl px-[5px] pt-[1px] cursor-pointer'>0</span>
         </div>
-        <div className='flex justify-center gap-2'>
-            <img src={profile1} alt="profile"  className='w-[15%] h-[25%] rounded-[50%]'/>
+        <div className='flex justify-center items-center  gap-2 w-[30%] cursor-pointer'>
+            <img src={profile1} alt="profile"  className='w-[21%] pt-2 h-[25%] rounded-[50%]'/>
             <div>
-              <h6>Jessi Rose</h6>
-              <p>Admin</p>
+              <h6 className='font-bold'>Jessi Rose</h6>
+              <p className='text-[13px]'>Admin</p>
             </div>
-            <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faChevronDown} className='text-[12px] pt-2 cursor-pointer' />
         </div>
       </div>
-      
     </div>
   )
 }
